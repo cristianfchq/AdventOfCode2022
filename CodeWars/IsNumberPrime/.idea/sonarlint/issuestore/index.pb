37f6551809a39bb44f39beddf875bea64d531fcc
cristianfchq <cristian.fchq@gmail.com>
2024-08-28 01:16:06 -0400

    add project idea to solve IsNumberPrime

---
+++ CodeWars/IsNumberPrime/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
+S
+#src/main/java/org/example/Main.java,0/b/0ba1b9c7a4878802f938e331cc2d8a042087ec6b
+U
+%src/main/java/org/example/Number.java,7/1/71688c0eff72a330f10101600929b13c5a417123
---
+++ CodeWars/IsNumberPrime/src/main/java/org/example/Number.java
@@ -0,0 +1,70 @@
+package org.example;
+
+public class Number {
+
+  private int number;
+
+  public Number() {
+  }
+
+  public Number(final int number) {
+    this.number = number;
+  }
+
+  public int getNumber() {
+    return number;
+  }
+
+  public void setNumber(final int number) {
+    this.number = number;
+  }
+
+  public boolean isPrime() {
+    int count = 0;
+    if (isGreaterThanOne()) {
+      for (int i = 1; i <= number; i++) {
+        System.out.println(i);
+        if (number % i == 0) {
+          count++;
+        }
+      }
+      if (count == 2) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  public boolean isPrime2() {
+    int nSquared = (int)Math.sqrt(number);
+    if (isGreaterThanOne()) {
+      for (int i = 2; i <= nSquared; i++) {
+        if (number % i == 0) {
+          return false;
+        }
+      }
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public boolean isPrime3() {
+    if (number <= 1) {
+      return false;
+    }
+    for (int i = 2; i <= Math.sqrt(number); i++) {
+      if (number % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public boolean isGreaterThanOne() {
+    return (number > 1) ? true : false;
+  }
+}
